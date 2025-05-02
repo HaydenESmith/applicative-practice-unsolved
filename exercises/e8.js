738f47504f76f3e207c63a3a88fb69303cc57263
Hayden Smith <ha.smith623@gmail.com>
2025-05-02 14:26:45 -0500

    halfway done

--- exercises/e8.js
+++ exercises/e8.js
@@ -6,9 +6,10 @@ import { data } from "../data/data";
 
 export function findPlanetNameByMoon(data, moonName) {
   // Your code goes here...
+  return data.planets.find(
+    (planet) => planet.moons && planet.moons.includes(moonName)
+  )?.name;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
